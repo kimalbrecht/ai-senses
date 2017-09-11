2428ea62fe24a61e1ed7ddfa5c47cad94dcff695
“Kim <kmlbrcht@gmail.com>
2017-09-11 12:26:45 +0200

    text changes

--- js/open-vis.js
+++ js/open-vis.js
@@ -2,27 +2,27 @@
 
 document.getElementById('touchingLiveButton').addEventListener('click', function() {
 	openInNewTab('touching.html')
-});
+}, true);
 
 document.getElementById('seeingLiveButton').addEventListener('click', function() {
 	openInNewTab('seeing.html')
-});
+}, true);
 
 document.getElementById('hearingLiveButton').addEventListener('click', function() {
 	openInNewTab('hearing.html')
-});
+}, true);
 
 document.getElementById('orientingLiveButton').addEventListener('click', function() {
 	openInNewTab('orienting.html')
-});
+}, true);
 
 document.getElementById('movingLiveButton').addEventListener('click', function() {
 	openInNewTab('moving.html')
-});
+}, true);
 
 document.getElementById('locatingLiveButton').addEventListener('click', function() {
 	openInNewTab('locating.html')
-});
+}, true);
 
 function openInNewTab(url) {
   var win = window.open(url, '_blank');
